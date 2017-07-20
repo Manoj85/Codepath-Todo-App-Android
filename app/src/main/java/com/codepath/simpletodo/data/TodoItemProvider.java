344b1f095613c88c448c8c3daaf99df37c3194ac
Manoj Talagadadeevi <manojt.854@gmail.com>
2017-07-19 22:32:49 -0400

    Fix issue with missing notifyChange() for the cursor

--- app/src/main/java/com/codepath/simpletodo/data/TodoItemProvider.java
+++ app/src/main/java/com/codepath/simpletodo/data/TodoItemProvider.java
@@ -108,6 +108,12 @@ public class TodoItemProvider extends ContentProvider {
                 throw new IllegalArgumentException("Cannot query unknown URI " + uri);
         }
 
+        // Set notification URI on the Cursor,
+        // so we know what content URI the Cursor was created for.
+        // If the data at this URI changes, then we know we need to update the cursor.
+        cursor.setNotificationUri(getContext().getContentResolver(), uri);
+
+        // Return the cursor
         return cursor;
     }
 
@@ -153,6 +159,10 @@ public class TodoItemProvider extends ContentProvider {
             Log.e(LOG_TAG, "Failed to insert row for " + uri);
             return null;
         }
+
+        // Notify all listeners that the data has changed for the item content URI
+        getContext().getContentResolver().notifyChange(uri, null);
+
 
         // Return the new URI with the ID (of the newly inserted row) appended at the end
         return ContentUris.withAppendedId(uri, newRowId);
@@ -212,9 +222,17 @@ public class TodoItemProvider extends ContentProvider {
         // Otherwise, get writeable database to update the data
         SQLiteDatabase db = mDbHelper.getWritableDatabase();
 
+        // Perform the update on the database and get the number of rows affected
+        int rowsUpdated = db.update(ItemEntry.TABLE_NAME, values, selection, selectionArgs);
 
-        // Return the number of rows that were affected
-        return db.update(ItemEntry.TABLE_NAME, values, selection, selectionArgs);
+        // If 1 or more rows were updated, then notify all listeners that the data at the
+        // given URI has changed
+        if (rowsUpdated != 0) {
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+
+        // Return the number of rows updated
+        return rowsUpdated;
     }
 
     /**
@@ -225,19 +243,33 @@ public class TodoItemProvider extends ContentProvider {
         // Get writeable database
         SQLiteDatabase db = mDbHelper.getWritableDatabase();
 
+        // Track the number of rows that were deleted
+        int rowsDeleted;
+
         final int match = sUriMatcher.match(uri);
         switch (match) {
             case TODOITEMS:
                 // Delete all rows that match the selection and selection args
-                return db.delete(ItemEntry.TABLE_NAME, selection, selectionArgs);
+                rowsDeleted = db.delete(ItemEntry.TABLE_NAME, selection, selectionArgs);
+                break;
             case TODOITEM_ID:
                 // Delete a single row given by the ID in the URI
                 selection = ItemEntry._ID + "=?";
                 selectionArgs = new String[] { String.valueOf(ContentUris.parseId(uri)) };
-                return db.delete(ItemEntry.TABLE_NAME, selection, selectionArgs);
+                rowsDeleted = db.delete(ItemEntry.TABLE_NAME, selection, selectionArgs);
+                break;
             default:
                 throw new IllegalArgumentException("Deletion is not supported for " + uri);
         }
+
+        // If 1 or more rows were deleted, then notify all listeners that the data at the
+        // given URI has changed
+        if (rowsDeleted != 0) {
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+
+        // Return the number of rows deleted
+        return rowsDeleted;
     }
 
     /**
